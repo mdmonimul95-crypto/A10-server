3149eb2398e9dc8391fd4af48b467bc3fa30948c
mdmonimul95-crypto <mdmonimul95@gmail.com>
2026-06-25 11:42:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+// const dns = require("node:dns");
+// dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+dotenv.config();
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("products_resell");
+    const productsCollection = database.collection("product");
+
+    app.post("/api/prompts", async (req, res) => {
+      const prompt = req.body;
+      const result = await productsCollection.insertOne(prompt);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+run();
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
